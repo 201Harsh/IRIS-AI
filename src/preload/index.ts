292b0887f71ab41e28d401e56693c9776fe9a9be
Harsh <pandeygautam039@gmail.com>
2026-02-07 23:01:16 +0530

    Updating Preload Scripts

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,7 +1,6 @@
 import { contextBridge, ipcRenderer } from 'electron'
 import { electronAPI } from '@electron-toolkit/preload'
 
-// Custom APIs for renderer
 const api = {}
 
 if (process.contextIsolated) {
