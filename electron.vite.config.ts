58fc1955a962e6c3ecdc91ec640186bb5aa3e5b3
Harsh <pandeygautam039@gmail.com>
2026-04-19 07:27:23 +0530

    update electron.vite.config.ts and eslint.config.mjs

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'electron-vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   main: {},
   preload: {},
